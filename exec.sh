7dbba97b5c66364706c69e46e884a773e87ed8fd
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-04-25 00:56:12 -0300

    adjust to make output format compatible to a CSV file and automation of the execution

--- exec.sh
+++ exec.sh
@@ -3,15 +3,15 @@
 
 # file existence
 out_file="output/results.csv"
+number=30
 
 if [ ! -f "$out_file" ]; then
-	echo "Execution time." > "$out_file"
-else
-	> "$out_file"
+	touch "$out_file"
+	echo "algorithm_name,number,time" > "$out_file"
 fi
 
 
-# compile
+# compile recursive file
 if gcc recursive_fib.c -o output/recursive_fib.out; then
 	echo "recursive_fib.c has been compiled."
 else
@@ -19,7 +19,7 @@ else
 	exit 1
 fi
 
-
+# compile iterative file
 if gcc iterative_fib.c -o output/iterative_fib.out; then
 	echo "iterative_fib.c has been compiled."
 else
@@ -27,30 +27,32 @@ else
 	exit 1
 fi
 
-echo ">Type the Fibonacci's number you want to calculate:"
-read n
+# run recursive file
+for ((i=1; i<number; i++))
+do
+	echo "recursive_fib($i) running..."
 
-echo "Iterative fibonacci: " >> "$out_file"
-echo "itertive_fib running..."
+	if ./output/recursive_fib.out "$i"; then
+		echo "Succefully executed."
+	else
+		echo "Unable to execute."
+		exit 1
+	fi
+done
 
-if ./output/iterative_fib.out "$n"; then
-	echo "Succefully executed."
-else
-	echo "Unable to execute."
-	exit 1
-fi
+# run iterative file
+for ((i=1; i<number; i++))
+do
+	echo "itertive_fib($i) running..."
 
+	if ./output/iterative_fib.out "$i"; then
+		echo "Succefully executed."
+	else
+		echo "Unable to execute."
+		exit 1
+	fi
+done
 
-echo "Recursive fibonacci: " >> "$out_file"
-echo "recursive_fib running..."
-
-if ./output/recursive_fib.out "$n"; then
-	echo "Succefully executed."
-else
-	echo "Unable to execute."
-	exit 1
-fi
-
-	echo -e "\n\n----------RESULTS----------"
+echo -e "\n\n----------RESULTS----------"
 cat output/results.csv
 
